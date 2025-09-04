29568e4e02fd4d0ac970db3f2f9b171c1b8bfc18
Yadiraluu <yulietlun24@gmail.com>
2025-09-03 22:54:31 -0500

    Agrego nueva carpeta al proyecto

---
+++ 24/script.js
@@ -0,0 +1,18 @@
+console.log('hola mundo');
+let fullName = 'Yadira Luna';
+console.log(fullName);
+
+let person;
+console.log(person)
+console.log(typeof(person));
+
+
+let valorVerdadero = true;
+ console.log(valorVerdadero);
+ console.log(typeof(valorVerdadero));
+
+ let peli = 'Era de Hielo';
+ 
+ let serie = 'Mas allá del Jardin'
+console.log(serie);
+console.log(typeof(serie));
